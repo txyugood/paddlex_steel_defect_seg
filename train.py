1c9ae523e076a9d713ae924758f4274637e9115b
tongxingyu <tongxingyu@ln.chinamobile.com>
2021-09-01 22:32:20 +0800

    Init source.

---
+++ train.py
@@ -0,0 +1,47 @@
+
+import paddlex as pdx
+from paddlex import transforms as T
+
+train_transforms = T.Compose([
+    T.Resize(target_size=512),
+    T.RandomHorizontalFlip(),
+    T.Normalize(
+        mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+])
+
+eval_transforms = T.Compose([
+    T.Resize(target_size=512),
+    T.Normalize(
+        mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+])
+
+train_dataset = pdx.datasets.SegDataset(
+                        data_dir='./crack_forest_seg',
+                        file_list='./crack_forest_seg/train_list.txt',
+                        label_list='./crack_forest_seg/labels.txt',
+                        transforms=train_transforms,
+                        shuffle=True)
+eval_dataset = pdx.datasets.SegDataset(
+                        data_dir='./crack_forest_seg',
+                        file_list='./crack_forest_seg/val_list.txt',
+                        label_list='crack_forest_seg/labels.txt',
+                        transforms=eval_transforms)
+
+# 初始化模型，并进行训练
+# 可使用VisualDL查看训练指标，参考https://paddlex.readthedocs.io/zh_CN/develop/train/visualdl.html
+num_classes = len(train_dataset.labels)
+
+# API说明：https://paddlex.readthedocs.io/zh_CN/develop/apis/models/semantic_segmentation.html#paddlex-seg-deeplabv3p
+model = pdx.seg.DeepLabV3P(
+    num_classes=num_classes, backbone='ResNet50_vd')
+
+# API说明：https://paddlex.readthedocs.io/zh_CN/develop/apis/models/semantic_segmentation.html#train
+# 各参数介绍与调整说明：https://paddlex.readthedocs.io/zh_CN/develop/appendix/parameters.html
+model.train(
+    num_epochs=40,
+    train_dataset=train_dataset,
+    train_batch_size=4,
+    eval_dataset=eval_dataset,
+    learning_rate=0.01,
+    save_dir='output/deeplabv3p_mobilenetv2',
+    use_vdl=True)
